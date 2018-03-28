bbab65f36bc274a4eda92a921312583c90c68835
Hunter Caron <huntercaron@mac.com>
2018-03-28 10:06:28 -0400

    menus!

--- public/electron.js
+++ public/electron.js
@@ -1,23 +1,77 @@
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
-const ipc = require('electron').ipcMain
+const Menu = electron.Menu;
+
 
 const path = require('path');
 const url = require('url');
 const isDev = require('electron-is-dev');
+const ipc = require('electron').ipcMain
 
 let mainWindow;
 
 function createWindow() {
   mainWindow = new BrowserWindow({
-    width: 500, height: 500, frame: false, webPreferences: {
+    width: 500, height: 500, frame: false, 
+    icon: path.join(__dirname, 'icon.png'),
+    webPreferences: {
       nodeIntegration: false
     }});
   mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
   mainWindow.on('closed', () => mainWindow = null);
   // mainWindow.webContents.openDevTools()
+  mainWindow.setBackgroundColor("#fff");
 
+  const template = [
+    {
+      label: 'Edit',
+      submenu: [
+        { role: 'undo' },
+        { role: 'redo' },
+        { type: 'separator' },
+        { role: 'cut' },
+        { role: 'copy' },
+        { role: 'paste' },
+        { role: 'pasteandmatchstyle' },
+        { role: 'delete' },
+        { role: 'selectall' }
+      ]
+    },
+    {
+      label: 'View',
+      submenu: [
+        { role: 'reload' },
+        { role: 'forcereload' },
+        { role: 'toggledevtools' },
+        { type: 'separator' },
+        { role: 'resetzoom' },
+        { role: 'zoomin' },
+        { role: 'zoomout' },
+        { type: 'separator' },
+        { role: 'togglefullscreen' }
+      ]
+    },
+    {
+      role: 'window',
+      submenu: [
+        { role: 'minimize' },
+        { role: 'close' }
+      ]
+    },
+    {
+      role: 'help',
+      submenu: [
+        {
+          label: 'Learn More',
+          click() { require('electron').shell.openExternal('https://electronjs.org') }
+        }
+      ]
+    }
+  ]
+
+  const menu = Menu.buildFromTemplate(template)
+  Menu.setApplicationMenu(menu)
 }
 
 
