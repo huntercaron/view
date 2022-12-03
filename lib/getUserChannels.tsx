c4a0d7b128d8a4ab9e5abf7700c470b474749c58
Hunter Caron <huntercaron@me.com>
2022-12-03 21:10:21 +0100

    Fix not logged in state

--- lib/getUserChannels.tsx
+++ lib/getUserChannels.tsx
@@ -9,6 +9,7 @@ interface ChannelResponse {
 }
 
 export async function getUserChannels(token: string): Promise<Arena.Channel[]> {
+    if (token) return null
     const userId = await getUserId(token)
 
     const res = await fetch(`http://api.are.na/v2/users/${userId}/channels`, {
